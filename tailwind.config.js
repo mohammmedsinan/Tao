c48986ffba4b8d7c9b73bb4f906eb7284972ee3d
mohammmedsinan <mohammedbusiness1099@gmail.com>
2025-01-12 06:41:49 +0300

    New Color Scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ export default {
   ],
   theme: {
     extend: {},
+    colors: {
+      MidNightDark: "#000957",
+      SkyBlue: "#344CB7",
+      LiteBlue: "#577BC1",
+      Sun: "#FFEB00"
+    }
   },
   plugins: [],
 }
